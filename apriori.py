01d2d68f1a50142629ccce7dbbd7f93d56b9424d
Jay Lal <jay.lal@somaiya.edu>
2017-09-04 17:27:12 +0530

    Completely Functional

--- apriori.py
+++ apriori.py
@@ -31,22 +31,12 @@ database = [(1,3,4),(2,3,5),(1,2,3,5),(2,5)]
 
 threshold = 2
 
-
 #Database is a list of sets/frozenset
 database = [frozenset(entry) for entry in database]
 
-# for d in database:
-# 	print(d)
-#######Database ready###############
-
-
-#Initial set of size 1
-
-#******************************************#
-
 item_set = dict()
 
-###########Occurences/Frequency Calculation###########3
+#Get initial frequency Count for every item in database
 for entry in database:
 	for item in entry:
 		item = frozenset([item])
@@ -55,28 +45,32 @@ for entry in database:
 		else:
 			item_set[item]=1
 
+print('***Initial Frequency Count***')
 for item in item_set:
 	print(item,':',item_set[item])	
 
+while(True):
+	#Create a list of elements/sets whose support is greater than threshold
+	frequent_items = prune(item_set,threshold)
 
-#############Create a list of elements/sets whose support is greater than threshold#################
-frequent_items = prune(item_set,threshold)
+	print('\n****Frequent_items****')
+	for f in frequent_items:
+		print(f)
 
-print('frequent_items')
-for f in frequent_items:
-	print(f)
+	#Stop if you only have 1 set/item left in frequent items
+	if len(frequent_items)==1:
+		break		
 
-######Stop if you only have 1 set/item left in frequent items##############
-if len(frequent_items)==1:
-	print('Game Over')
-	#return
+	#Make sets using the sets/items from this list 
+	new_item_set = combine_sets(frequent_items)
+	#print(new_item_set)		
 
-########Make sets using the sets/items from this list############# 
+	#Get the count/frequency of new items/sets in the database
+	item_set = get_count(new_item_set,database)
+	#print(item_set)
 
-new_item_set = combine_sets(frequent_items)
-print(new_item_set)		
-
-#######Count their occurences##############
-
-item_set = get_count(new_item_set,database)
-print(item_set)
+print('***************************************************************')
+print('The most frequenty associated items are:')
+for item in frequent_items:
+	print(item,end=' ')
+print('\n')
